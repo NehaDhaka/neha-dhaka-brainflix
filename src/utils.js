6fbdc9ebfa7f1f6a7fac3f9e6d4ca53b9ecf0c9c
Neha Dhaka <nehadhaka43@gmail.com>
2023-05-20 22:33:09 -0400

    create utils.js file to contain formatTimeStamp function

---
+++ src/utils.js
@@ -0,0 +1,27 @@
+export function formatTimeStamp(timeStamp) {
+  const now = new Date();
+  const diff = Math.floor((now.getTime() - timeStamp) / 1000);
+  if (diff < 60) {
+    return `${diff} second${diff > 1 ? "s" : ""} ago`;
+  } else if (diff < 3600) {
+    return `${Math.floor(diff / 60)} minute${
+      Math.floor(diff / 60) > 1 ? "s" : ""
+    } ago`;
+  } else if (diff < 86400) {
+    return `${Math.floor(diff / 3600)} hour${
+      Math.floor(diff / 3600) > 1 ? "s" : ""
+    } ago`;
+  } else if (diff < 86400 * 30) {
+    return `${Math.floor(diff / 86400)} day${
+      Math.floor(diff / 86400) > 1 ? "s" : ""
+    } ago`;
+  } else if (diff < 86400 * 30 * 12) {
+    return `${Math.floor(diff / (86400 * 30))} month${
+      Math.floor(diff / (86400 * 30)) > 1 ? "s" : ""
+    } ago`;
+  } else {
+    return `${Math.floor(diff / (86400 * 30 * 12))} year${
+      Math.floor(diff / (86400 * 30 * 12)) > 1 ? "s" : ""
+    } ago`;
+  }
+}
