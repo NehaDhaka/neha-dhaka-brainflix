bb3427fa65a41562eff9b57e86e71bf93840fc9c
Neha Dhaka <nehadhaka43@gmail.com>
2023-05-20 22:21:48 -0400

    create component - DetailsCommentOld

--- src/components/MainC/MainC.jsx
+++ src/components/MainC/MainC.jsx
@@ -6,18 +6,18 @@ import videoDetails from "../../data/video-details.json";
 import { useState } from "react";
 
 export default function Main() {
-  const [currentPoster, setCurrentPoster] = useState(videoDetails[0]);
+  // const [currentPoster, setCurrentPoster] = useState(videoDetails[0]);
 
-  const filteredList = videoList.filter((video) => {
-    return video.id !== currentPoster.id;
-  });
+  // const filteredList = videoList.filter((video) => {
+  //   return video.id !== currentPoster.id;
+  // });
 
-  const updateCurrentVideo = function (id) {
-    const updatedCurrentPoster = videoDetails.find((video) => {
-      return id === video.id;
-    });
-    setCurrentPoster(updatedCurrentPoster);
-  };
+  // const updateCurrentVideo = function (id) {
+  //   const updatedCurrentPoster = videoDetails.find((video) => {
+  //     return id === video.id;
+  //   });
+  //   setCurrentPoster(updatedCurrentPoster);
+  // };
   return (
     <main className="main">
       <CurrentPoster currentPoster={currentPoster} />
