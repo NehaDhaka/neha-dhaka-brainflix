f930675e684ef7216c1327af5b916757591fbf1e
Neha Dhaka <nehadhaka43@gmail.com>
2023-05-24 21:43:47 -0400

    test commit

--- src/components/MainC/MainC.jsx
+++ src/components/MainC/MainC.jsx
@@ -5,13 +5,14 @@ import videoList from "../../data/videos.json";
 import videoDetails from "../../data/video-details.json";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { api } from "../src/utils/api";
+import { api } from "../../utils/api";
 
 export default function Main() {
-  useEffect(() => {
-    axios.get(api).then((response) => console.log(response.data));
-  }, []);
   const [currentPoster, setCurrentPoster] = useState(videoDetails[0]);
+  console.log(videoDetails[0]);
+  useEffect(() => {
+    axios.get(api).then((response) => console.log(response.data[0]));
+  }, []);
 
   const filteredList = videoList.filter((video) => {
     return video.id !== currentPoster.id;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
-const videosURL = "https://project-2-api.herokuapp.com/videos";
+const videosURL =
+  "https://project-2-api.herokuapp.com/videos/84e96018-4022-434e-80bf-000ce4cd12b8";
 const apiKey = "e77287ff-41d5-4bd1-a7f7-d453a66f92a3";
 export const api = `${videosURL}?api_key=${apiKey}`;
