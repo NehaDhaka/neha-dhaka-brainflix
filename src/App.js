0c50d6b457ba09fd7293f7f62d99eb3be1301c56
Neha Dhaka <nehadhaka43@gmail.com>
2023-05-24 21:32:10 -0400

    make get request using axios

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import Header from "./components/Header/Header";
 import MainC from "./components/MainC/MainC";
 import UploadVideo from "./pages/UploadVideo/UploadVideo";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
-import { api } from "../src/utils/api";
 
 function App() {
-  console.log(api);
   return (
     <>
       <BrowserRouter>
--- src/components/MainC/MainC.jsx
+++ src/components/MainC/MainC.jsx
@@ -3,9 +3,14 @@ import Details from "../Details/Details";
 import "./MainC.scss";
 import videoList from "../../data/videos.json";
 import videoDetails from "../../data/video-details.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { api } from "../src/utils/api";
 
 export default function Main() {
+  useEffect(() => {
+    axios.get(api).then((response) => console.log(response.data));
+  }, []);
   const [currentPoster, setCurrentPoster] = useState(videoDetails[0]);
 
   const filteredList = videoList.filter((video) => {
